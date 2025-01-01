475b0c68082b142148f69a11c43841fc24ca5186
itsAman-24 <amanpan2410@gmail.com>
2025-01-01 19:39:11 +0530

    project completed

---
+++ backend/src/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js"
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+import { app, server } from "./lib/socket.js";
+import path from "path";
+
+// Allow requests from your frontend origin
+// const corsOptions = {
+//   origin: 'http://localhost:5173',  // Replace with your frontend URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,  // Allow cookies to be sent with requests
+// };
+
+
+dotenv.config();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+
+
+// app.use(cors(corsOptions));
+app.use(cookieParser());
+// Increase the body size limit
+app.use(express.json({ limit: '10mb' })); // For JSON payloads
+// app.use(express.urlencoded({ extended: true }));
+app.use(cors( {
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+// app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
+  
+
+server.listen(PORT, () => {
+    console.log("server is running on port:" + PORT);
+    connectDB();
+})
+
+
+// YouTube = TimeStamp = 1:21:02
