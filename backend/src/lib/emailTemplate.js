b7f2b9a774f69a14204ea7eb55f7eb955602a21b
itsAman-24 <amanpan2410@gmail.com>
2025-01-18 08:22:05 +0530

    Added

--- backend/src/lib/emailTemplate.js
+++ backend/src/lib/emailTemplate.js
@@ -158,14 +158,11 @@ export const Welcome_Email_Template = `
               <p>Here’s how you can get started:</p>
               <ul>
                   <li>Explore our features and customize your experience.</li>
-                  <li>Stay informed by checking out our blog for the latest updates and tips.</li>
                   <li>Reach out to our support team if you have any questions or need assistance.</li>
               </ul>
-              <a href="#" class="button">Get Started</a>
-              <p>If you need any help, don’t hesitate to contact us. We’re here to support you every step of the way.</p>
           </div>
           <div class="footer">
-              <p>&copy; ${new Date().getFullYear()} Your Company. All rights reserved.</p>
+              <p>&copy; ${new Date().getFullYear()} Chatty. All rights reserved.</p>
           </div>
       </div>
   </body>
